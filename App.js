5ca41f9195a383cccde2714ce9fe13a87ef7bf70
Declan Deng <qq2633389934@gmail.com>
2024-01-16 19:47:43 +0800

    feat: add login page

---
+++ App.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import {Text, View} from 'react-native';
+// import Index from './src_01_StyleSheet';
+import Index from './src_02_Flexbox/FlexDirection';
+
+export default class App extends Component {
+  render() {
+    return <Index />;
+  }
+}
